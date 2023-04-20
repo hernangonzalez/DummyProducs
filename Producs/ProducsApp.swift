31decc906a4740dba487c3448ca6f23b3d16beaf
Hernán G. Gonzalez <camus.sonico@gmail.com>
2023-04-20 09:44:13 +0100

    Initial Commit

---
+++ Producs/ProducsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProducsApp.swift
+//  Producs
+//
+//  Created by Hernan G. Gonzalez on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProducsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
